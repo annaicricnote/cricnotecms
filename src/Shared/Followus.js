d890b9d32e0db5b927a8f10e9948d02d9fdb893a
annai <annaimahalingam.cricnote@gmail.com>
2023-06-19 16:01:30 +0530

    follow link update

--- src/Shared/Followus.js
+++ src/Shared/Followus.js
@@ -9,28 +9,28 @@ function Followus() {
 
                               <div className="row text-center">
                                   <div className="col-lg-3 col-md-3 col-sm-3 col-xs-6">
-                                        <a href="#"    className="social-button facebook-button">
+                                        <a href="https://www.facebook.com/profile.php?id=100093977225980"  target="_blank"  className="social-button facebook-button">
                                             <i    className="fa fa-facebook"></i>
                                             <p>5</p>
                                         </a>
                                     </div>
 
                                   <div className="col-lg-3 col-md-3 col-sm-3 col-xs-6">
-                                        <a href="https://twitter.com/_cricnote" className="social-button twitter-button">
+                                        <a href="https://twitter.com/_cricnote" target="_blank"  className="social-button twitter-button">
                                             <i    className="fa fa-twitter"></i>
                                             <p>2</p>
                                         </a>
                                     </div>
 
-                                  {/* <div className="col-lg-3 col-md-3 col-sm-3 col-xs-6">
-                                        <a href="#"    className="social-button google-button">
-                                            <i    className="fa fa-google-plus"></i>
+                                <div className="col-lg-3 col-md-3 col-sm-3 col-xs-6">
+                                        <a href="https://www.instagram.com/_cricnote/"  target="_blank"   className="social-button google-button">
+                                            <i    className="fa fa-instagram"></i>
                                             <p>1</p>
                                         </a>
-                                    </div> */}
+                                    </div> 
 
                                   <div className="col-lg-3 col-md-3 col-sm-3 col-xs-6">
-                                        <a href="#"    className="social-button youtube-button">
+                                        <a href="https://www.youtube.com/@Cricnote-xp3ht"    target="_blank"  className="social-button youtube-button">
                                             <i    className="fa fa-youtube"></i>
                                             <p>6</p>
                                         </a>
